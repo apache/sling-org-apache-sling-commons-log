70f44c8d7f6889a1065268f0716ec56691268ef1
Chetan Mehrotra <chetanm@apache.org>
2017-10-31 19:46:35 +0530

    SLING-3049 - Make Logback Stacktrace Packaging data support OSGi aware
    Ensure that bundle info is only provided if there is a single owner of
    any package. If same package is provided by multiple bundles then no bundle
    info would be provided

--- src/test/java/org/apache/sling/commons/log/logback/internal/stacktrace/PackageInfoCollectorTest.java
+++ src/test/java/org/apache/sling/commons/log/logback/internal/stacktrace/PackageInfoCollectorTest.java
@@ -20,14 +20,60 @@
 package org.apache.sling.commons.log.logback.internal.stacktrace;
 
 import org.junit.Test;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.Version;
+import org.osgi.framework.hooks.weaving.WovenClass;
+import org.osgi.framework.wiring.BundleWiring;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class PackageInfoCollectorTest {
+    private PackageInfoCollector collector = new PackageInfoCollector();
+
     @Test
     public void packageName() throws Exception{
         assertEquals("com.foo", PackageInfoCollector.getPackageName("com.foo.TestClass"));
         assertEquals("", PackageInfoCollector.getPackageName("packageInDefaultClass"));
     }
 
+    @Test
+    public void getBundleInfo() throws Exception{
+        Bundle bundle = newBundle("foo.bundle", "0.0.7");
+        collector.weave(newWovenClass(bundle, "com.example.Foo"));
+
+        assertEquals("foo.bundle:0.0.7",collector.getBundleInfo("com.example.Foo"));
+        assertEquals("foo.bundle:0.0.7",collector.getBundleInfo("com.example.Bar"));
+        assertNull(collector.getBundleInfo("com.example2.Bar"));
+    }
+
+    /**
+     * For case where same package is present in multiple bundles then no bundle info is provided
+     */
+    @Test
+    public void duplicates() throws Exception{
+        collector.weave(newWovenClass(newBundle("foo.bundle", "0.0.7"), "com.example.Foo"));
+        collector.weave(newWovenClass(newBundle("foo.bundle2", "0.0.8"), "com.example.Bar"));
+
+        assertNull(collector.getBundleInfo("com.example.Bar"));
+    }
+
+    private Bundle newBundle(String name, String version){
+        Bundle b = mock(Bundle.class);
+        when(b.getSymbolicName()).thenReturn(name);
+        when(b.getVersion()).thenReturn(Version.parseVersion(version));
+        return b;
+    }
+
+    private WovenClass newWovenClass(Bundle bundle, String className){
+        WovenClass woven = mock(WovenClass.class);
+        BundleWiring wiring = mock(BundleWiring.class);
+        when(woven.getBundleWiring()).thenReturn(wiring);
+
+        when(wiring.getBundle()).thenReturn(bundle);
+        when(woven.getClassName()).thenReturn(className);
+        return woven;
+    }
 }

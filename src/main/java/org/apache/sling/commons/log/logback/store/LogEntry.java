a64cc1774996f9d26cdf2db8716ee57504fa89db
Robert Munteanu <rombert@apache.org>
2026-05-21 15:09:14 +0200

    SLING-13188 - Provide an optional in-memory store of recent log events
    Make LogEntry a record.

--- src/main/java/org/apache/sling/commons/log/logback/store/LogEntry.java
+++ src/main/java/org/apache/sling/commons/log/logback/store/LogEntry.java
@@ -21,7 +21,6 @@ package org.apache.sling.commons.log.logback.store;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * Snapshot of a log entry.
@@ -29,88 +28,16 @@ import java.util.Objects;
  * <p>Stores only the lightweight, stable parts of a log event so the log store
  * does not retain full logging event object graphs.</p>
  */
-public final class LogEntry {
+public record LogEntry(
+        long timeMillis,
+        LogLevel level,
+        String loggerName,
+        String threadName,
+        String formattedMessage,
+        String throwableText,
+        Map<String, String> mdc) {
 
-    private final long timeMillis;
-    private final LogLevel level;
-    private final String loggerName;
-    private final String threadName;
-    private final String formattedMessage;
-    private final String throwableText;
-    private final Map<String, String> mdc;
-
-    public LogEntry(
-            long timeMillis,
-            LogLevel level,
-            String loggerName,
-            String threadName,
-            String formattedMessage,
-            String throwableText,
-            Map<String, String> mdc) {
-        this.timeMillis = timeMillis;
-        this.level = level;
-        this.loggerName = loggerName;
-        this.threadName = threadName;
-        this.formattedMessage = formattedMessage;
-        this.throwableText = throwableText;
-        this.mdc = mdc.isEmpty() ? Collections.emptyMap() : Collections.unmodifiableMap(new HashMap<>(mdc));
-    }
-
-    public long timeMillis() {
-        return timeMillis;
-    }
-
-    public LogLevel level() {
-        return level;
-    }
-
-    public String loggerName() {
-        return loggerName;
-    }
-
-    public String threadName() {
-        return threadName;
-    }
-
-    public String formattedMessage() {
-        return formattedMessage;
-    }
-
-    public String throwableText() {
-        return throwableText;
-    }
-
-    public Map<String, String> mdc() {
-        return mdc;
-    }
-
-    @Override
-    public boolean equals(Object other) {
-        if (this == other) {
-            return true;
-        }
-        if (!(other instanceof LogEntry)) {
-            return false;
-        }
-        LogEntry that = (LogEntry) other;
-        return timeMillis == that.timeMillis
-                && Objects.equals(level, that.level)
-                && Objects.equals(loggerName, that.loggerName)
-                && Objects.equals(threadName, that.threadName)
-                && Objects.equals(formattedMessage, that.formattedMessage)
-                && Objects.equals(throwableText, that.throwableText)
-                && Objects.equals(mdc, that.mdc);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(timeMillis, level, loggerName, threadName, formattedMessage, throwableText, mdc);
-    }
-
-    @Override
-    public String toString() {
-        return "LogEntry{" + "timeMillis=" + timeMillis + ", level='" + level + '\'' + ", loggerName='"
-                + loggerName + '\'' + ", threadName='" + threadName + '\'' + ", formattedMessage='"
-                + formattedMessage + '\'' + ", throwableText='" + throwableText + '\'' + ", mdc=" + mdc + '}';
+    public LogEntry {
+        mdc = mdc.isEmpty() ? Collections.emptyMap() : Collections.unmodifiableMap(new HashMap<>(mdc));
     }
 }
